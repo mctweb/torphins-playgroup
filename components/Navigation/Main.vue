f34eebda5d9fbcb1b2af331de7ac3374900badb4
Fraser McTaggart <fraser@mctweb.co.uk>
2022-05-26 16:37:21 +0100

    fix: relative path to logo

--- components/Navigation/Main.vue
+++ components/Navigation/Main.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="bg-white bg-blend-screen  flex bg-opacity-50 text-blue w-full px-12 top-0 z-50 fixed backdrop-filter backdrop-blur-lg justify-between items-center">
     <header class="h-24 py-6">
-      <img src="Logo-Text.svg" class="h-full w-auto">
+      <img src="/Logo-Text.svg" class="h-full w-auto">
     </header>
     <section class="flex  h-24 z-50 items-center justify-center lg:hidden">
       <NavigationIcon :open="navigationOpen" @click="navigationOpen = !navigationOpen" />
