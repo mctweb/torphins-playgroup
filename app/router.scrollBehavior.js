5875cda796a924c0b182fca54eeaa0a6068dce98
Fraser McTaggart <fraser@mctweb.co.uk>
2022-06-08 21:42:59 +0100

    feat: smooth scrolling

---
+++ app/router.scrollBehavior.js
@@ -0,0 +1,22 @@
+export default function (to, from, savedPosition) {
+  console.log('this is the hash', to.hash)
+  return new Promise((resolve, reject) => {
+    if (to.hash) {
+      setTimeout(() => {
+        resolve({
+          selector: to.hash,
+          offset: { x: 0, y: 150 },
+          behavior: 'smooth'
+        })
+      }, 10)
+    } else {
+      setTimeout(() => {
+        resolve({
+          x: 0,
+          y: 0,
+          behavior: 'smooth'
+        })
+      }, 10)
+    }
+  })
+}
